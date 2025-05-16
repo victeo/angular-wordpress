7fca0742146de18bf4b160c18c35fe4b074be785
victeo <victor.atomo@gmail.com>
2025-05-16 15:20:35 -0300

    início do projeto

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://localhost/wordpress/wp-json/wp/v2'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  baseUrl: 'http://localhost/wordpress/wp-json/wp/v2'
+};
+
+
